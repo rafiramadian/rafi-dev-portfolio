e0212fc84871df7f9689f01be4e022730c64866a
Rafi Ramadian <89655817+rafiramadian@users.noreply.github.com>
2025-07-16 15:29:33 +0700

    update tailwind config for theming

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const config = {
+  darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
